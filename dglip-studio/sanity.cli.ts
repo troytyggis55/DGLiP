4062b50025c8f30456152e4129f8d6cd588447d1
Trygve Jørgensen <trygveabjo@gmail.com>
2025-10-26 22:13:52 +0100

    Configuration fixes

--- dglip-studio/sanity.cli.ts
+++ dglip-studio/sanity.cli.ts
@@ -5,9 +5,7 @@ export default defineCliConfig({
     projectId: 'j9xh26we',
     dataset: 'production'
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
+  deployment: {
+      autoUpdates: true
+  },
 })
